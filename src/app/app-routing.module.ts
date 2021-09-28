c028b3377e3f57c12106c2c1cc8051413e4f8b1e
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-09-28 18:37:18 -0500

    feat: test routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test',
+    component: NavComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
